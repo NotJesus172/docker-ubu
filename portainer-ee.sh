c817f65fbe712841970a855b2145ed8801249775
NotJesus172 <157871959+NotJesus172@users.noreply.github.com>
2024-10-11 02:55:15 -0400

    1.0.1

--- portainer-ee.sh
+++ portainer-ee.sh
@@ -4,4 +4,4 @@
 echo "Installing the portainer-ee:lts Docker container."
 #	Run the portainer server docker container:
 sudo docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:lts
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:lts

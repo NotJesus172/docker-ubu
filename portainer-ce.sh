bf24bc2276e74f0b94f155baf8ed7d8edc0f3f22
NotJesus172 <157871959+NotJesus172@users.noreply.github.com>
2024-10-11 03:09:45 -0400

    1.0.1

--- portainer-ce.sh
+++ portainer-ce.sh
@@ -2,6 +2,15 @@
 
 #   Script header:
 echo "Installing the portainer-ce:lts Docker container."
-#	Run the portainer server docker container:
+#	Pull the portainer-ce:lts docker container image:
+sudo docker pull portainer/portainer-ce:lts
+#	Run the portainer-ce:lts docker container:
 sudo docker volume create portainer_data
-sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
+sudo docker run -d \
+	-p 8000:8000 \
+	-p 9443:9443 \
+	--name portainer \
+	--restart=always \
+	-v /var/run/docker.sock:/var/run/docker.sock \
+	-v portainer_data:/data \
+	portainer/portainer-ce:lts

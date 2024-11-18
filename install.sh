20fdb126b77bd4337144c955d47817d1662fd13a
NotJesus172 <157871959+NotJesus172@users.noreply.github.com>
2024-11-17 22:41:19 -0500

    Adding Basic Utilities

--- install.sh
+++ install.sh
@@ -5,8 +5,10 @@ for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker c
 #	Update and install nala:
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install -y nala
-#	Update and upgrade the system:
+#	Add repos and install basic utilities:
+sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
 sudo nala update && sudo nala upgrade -y
+sudo nala install -y fastfetch konsole sl curl wget
 #	Add Docker's official GPG key:
 sudo nala install -y curl ca-certificates
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -15,8 +17,7 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 #	Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo nala update
 #	Install the Docker packages:
 sudo nala install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
